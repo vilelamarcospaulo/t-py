ca3258f7f6e33a9065cf5f1edd1349f67250c794
vilelamarcospaulo <marcos.vilela@quintoandar.com.br>
2021-07-19 22:18:39 -0300

    feat: create action to loot arroud player

---
+++ actions/lootbox.py
@@ -0,0 +1,30 @@
+import time
+import random
+
+class LootBox:
+    def __init__(self, charScreenPosition, offSet, mouseHandler):
+        self.charX, self.charY = charScreenPosition
+        self.offsetX, self.offsetY = offSet
+        self.mouseHandler = mouseHandler
+
+    def exec(self):
+        positions = self._calc_box_positions()
+        random.shuffle(positions)
+        
+        for pos in positions:
+            self.mouseHandler.click(pos)
+            rand_delay = random.uniform(0.09, 0.12)
+            time.sleep(rand_delay)
+        
+    def _calc_box_positions(self): 
+        return [
+            (self.charX, self.charY),                               ## CENTER
+            (self.charX + self.offsetX, self.charY),                ## EAST
+            (self.charX + self.offsetX, self.charY + self.offsetY), ## SOUTH-EAST 
+            (self.charX, self.charY + self.offsetY),                ## SOUTH
+            (self.charX - self.offsetX, self.charY + self.offsetY), ## SOUTH-WEST
+            (self.charX - self.offsetX, self.charY),                ## WEST
+            (self.charX - self.offsetX, self.charY - self.offsetY), ## NORTH-WEST
+            (self.charX, self.charY - self.offsetY),                ## NORTH
+            (self.charX + self.offsetX, self.charY - self.offsetY), ## NORTH-EAST
+        ]
