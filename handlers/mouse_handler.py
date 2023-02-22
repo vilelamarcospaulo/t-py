b7d4b1eb366bee5e94a7335f14c23d2ef8e11aef
marcos vilela <marcospaulo.vilelasantos@gmail.com>
2023-02-22 15:27:05 -0300

    feat: add capacity to drag using mouse handler

--- handlers/mouse_handler.py
+++ handlers/mouse_handler.py
@@ -1,14 +1,27 @@
+import time
 from AppKit import NSEvent
-from Quartz import CGEventPost, CGEventCreateMouseEvent, CGDisplayPixelsHigh, kCGHIDEventTap, kCGEventMouseMoved, kCGEventLeftMouseDown, kCGEventLeftMouseUp, kCGEventRightMouseDown, kCGEventRightMouseUp
+from Quartz import CGEventPost, CGEventCreateKeyboardEvent, CGEventCreateMouseEvent, CGDisplayPixelsHigh, kCGHIDEventTap, kCGEventMouseMoved, kCGEventLeftMouseDown, kCGEventLeftMouseUp, kCGEventRightMouseDown, kCGEventRightMouseUp
 
 class MouseHandler:
     def __init__(self, button):
         self.button = self._button(button)
         
     def click(self, position):
-        self._move(position)
+        self.move(position)
         self._press(position)
         self._release(position)
+
+    def drag(self, to):
+        current = self.current_position()
+        
+        self.move(current)
+        self._press(current)
+        self.move(to)
+        self._release(to)
+
+    def move(self, position):
+        move = CGEventCreateMouseEvent(None, kCGEventMouseMoved, position, 0)
+        CGEventPost(kCGHIDEventTap, move)
 
     def current_position(self):
         pos = NSEvent.mouseLocation()
@@ -27,10 +40,6 @@ class MouseHandler:
             return [kCGEventLeftMouseDown, kCGEventLeftMouseUp]
         else:
             return [kCGEventRightMouseDown, kCGEventRightMouseUp]
-        
-    def _move(self, position):
-        move = CGEventCreateMouseEvent(None, kCGEventMouseMoved, position, 0)
-        CGEventPost(kCGHIDEventTap, move)
 
     def _press(self, position):
         event = CGEventCreateMouseEvent(None, self._button_event_order()[0], position, self.button)
