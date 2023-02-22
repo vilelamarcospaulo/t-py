bee457cc6dcdcd8c3236117a9b33208a8da3dc1d
marcos vilela <marcospaulo.vilelasantos@gmail.com>
2023-02-22 15:45:20 -0300

    feat: uses cv to detect item name on console

---
+++ actions/sort_loot/messages.py
@@ -0,0 +1,36 @@
+import re
+import cv2
+from computer_vision.image_text_position import ImageTextPosition
+
+def show(img):
+  cv2.imshow("Image", img)
+  cv2.waitKey(0)
+  cv2.destroyAllWindows()
+
+def messages(image):
+  bottom_bar_height = 50
+  messages_panel_height = 100
+
+  messages_height = messages_panel_height + bottom_bar_height
+
+  hImg, wImg = image.shape
+
+  cropY = hImg - messages_height
+  cropX = 0
+  
+  cropped = image[cropY:cropY + messages_height, cropX:cropX + wImg].copy()
+  
+  image_reader = ImageTextPosition()
+  image_reader.process_image(cropped)
+
+  result = []
+  for text, _ in image_reader.flush():
+    
+    matches = re.search(r".* You see (a )?([a-z A-Z]*)( \(.*)?.", text)
+    if matches is None: 
+      continue
+
+    item_name = matches.groups()[1]
+    result.append(item_name)
+  
+  return result
