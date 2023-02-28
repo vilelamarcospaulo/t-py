74db1424531a971fa27f3770046e61a68e0c1dda
marcos vilela <marcospaulo.vilelasantos@gmail.com>
2023-02-27 22:49:29 -0300

    feat: register sort loot as a mouse event

--- executor.py
+++ executor.py
@@ -4,6 +4,7 @@ from Foundation import NSBundle
 from PyObjCTools import AppHelper
 
 from actions.lootbox import LootBox
+from actions.sort_loot.sort_loot import SortLoot
 from handlers.mouse_handler import MouseHandler
 from listeners.mouse_click_event_listener import MouseClickEventListener
 
@@ -11,14 +12,18 @@ CHARACTER_POSITION = (2671, 545)
 PIXEL_OFFSET = (60, 60)
 
 BUTTON_WHELL = 2
+BUTTON_SORT_LOOT = 5
 BUTTON_LEFT_FIRE = 4
 
 mouseHandler = MouseHandler('right')
 lootBox = LootBox(CHARACTER_POSITION, PIXEL_OFFSET, mouseHandler)
 
+sort_loot = SortLoot()
+
 eventHandler = MouseClickEventListener()
-eventHandler.on(BUTTON_WHELL, lambda event : print(mouseHandler.current_position()))
+eventHandler.on(BUTTON_WHELL, lambda event : lootBox.set_position(mouseHandler.current_position()))
 eventHandler.on(BUTTON_LEFT_FIRE, lambda event : lootBox.exec())
+eventHandler.on(BUTTON_SORT_LOOT, lambda event : sort_loot.run())
 
 class AppDelegate(NSObject):
     def applicationDidFinishLaunching_(self, notification):
