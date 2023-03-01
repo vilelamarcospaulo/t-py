aeae51b41479b7a7a90e9734fda76229a945f56a
marcos vilela <marcospaulo.vilelasantos@gmail.com>
2023-03-01 00:06:00 -0300

    feat: check item_name using string diff over only regex

--- actions/sort_loot/messages.py
+++ actions/sort_loot/messages.py
@@ -1,15 +1,15 @@
 import re
 import cv2
-from computer_vision.image_text_position import ImageTextPosition
+from computer_vision.image_text_position import ImageTextPosition, TextMatcher
 
 def show(img):
   cv2.imshow("Image", img)
   cv2.waitKey(0)
   cv2.destroyAllWindows()
 
-def messages(image):
+def messages(image, possible_names):
   bottom_bar_height = 50
-  messages_panel_height = 100
+  messages_panel_height = 110
 
   messages_height = messages_panel_height + bottom_bar_height
 
@@ -24,13 +24,20 @@ def messages(image):
   image_reader.process_image(cropped)
 
   result = []
+  text_matcher = TextMatcher(possible_names)
+
   for text, _ in image_reader.flush():
-    
-    matches = re.search(r".* You see (an|a )?([a-z A-Z]*)( \(.*)?.?", text)
+    matches = re.search(r"You see (a|an) (((?!that|\()([a-zA-Z]+)| )*)", text)
+    print(f'regex fail {text}')
     if matches is None: 
       continue
 
-    item_name = matches.groups()[1]
+    text = matches.groups()[2]
+    item_name = text_matcher.check_text(text)
+    print(f'register item fail {text}')
+    if item_name is None:
+        continue
+
     result.append(item_name)
   
   return result
